c74e46a565343013f9d7d0bb1d5b1ec032715d94
Vladislav Kozhushko <bagrusss@gmail.com>
2018-06-19 16:58:44 +0300

    clean + mvvm example

---
+++ CleanMvvm/app/src/main/java/ru/bagrusss/clean_mvvm/mvvm/DefaultViewModel.kt
@@ -0,0 +1,7 @@
+package ru.bagrusss.clean_mvvm.mvvm
+
+import android.arch.lifecycle.ViewModel
+
+abstract class DefaultViewModel: ViewModel() {
+
+}
---
+++ CleanMvvm/app/src/main/java/ru/bagrusss/clean_mvvm/mvvm/LifecycleHandler.kt
@@ -0,0 +1,7 @@
+package ru.bagrusss.clean_mvvm.mvvm
+
+import android.arch.lifecycle.LifecycleObserver
+
+class LifecycleHandler: LifecycleObserver {
+
+}
